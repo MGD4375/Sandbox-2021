851dea9056bac0ff46315f87242ffab3170d19f1
SCMorgan <Morgan@schoolcomms.co.uk>
2021-03-09 20:53:20 +0000

    game of life

--- rs-game-of-life_mod/www/app.js
+++ rs-game-of-life_mod/www/app.js
@@ -1,18 +1,56 @@
 import init, {
-    add,
-    greet
+    Universe
 } from '../pkg/game_of_life.js';
+import CanvasRenderer from './renderer.js';
 
-async function run() {
-    await init();
-    const result = add(1, 2);
-    console.log(`1 + 2 = ${result}`);
-    if (result !== 3) {
-        throw new Error("wasm addition doesn't work!");
-    }
+const WIDTH = 360;
+const HEIGHT = 360;
+const renderer = new CanvasRenderer(WIDTH, HEIGHT, 1);
 
-    greet("Morgan");
+!async function run() {
+    console.log('Begin')
 
-}
+    const wasm = await init();
+    const memory = wasm.memory;
 
-run();
+    const bitIsSet = (n, arr) => {
+        const byte = Math.floor(n / 8);
+        const mask = 1 << (n % 8);
+        return (arr[byte] & mask) === mask;
+    };
+
+
+    const universe = Universe.new(WIDTH, HEIGHT);
+    const width = universe.width();
+    const height = universe.height();
+
+    const renderLoop = () => {
+        drawGrid();
+        drawCells();
+
+        for (let i = 0; i < 9; i++) {
+            universe.tick();
+        }
+
+        requestAnimationFrame(renderLoop);
+    };
+
+    const drawGrid = () => {
+
+    };
+
+    const getIndex = (row, column) => {
+        return row * width + column;
+    };
+
+    const drawCells = () => {
+        const cellsPtr = universe.cells();
+        const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8);
+        cells.getIndex = getIndex;
+        cells.bitIsSet = bitIsSet;
+        renderer.render(cells);
+    };
+
+    renderLoop()
+
+}();
---
+++ rs-game-of-life_mod/www/renderer.js
@@ -0,0 +1,38 @@
+//  This is taken from my older js only game of life.
+export default class CanvasRenderer {
+    constructor(height, width, scale) {
+        this.height = height;
+        this.width = width;
+        this.canvas = document.getElementById("canvas");
+        this.canvas.height = height;
+        this.canvas.width = width;
+        this.canvasContext = this.canvas.getContext('2d');
+        this.scale = scale;
+    }
+
+    render(board) {
+        var imageData = new ImageData(this.canvas.width, this.canvas.height)
+
+        for (let colIndex = 0; colIndex < this.width; colIndex++) {
+            for (let rowIndex = 0; rowIndex < this.height; rowIndex++) {
+
+                const idx = board.getIndex(rowIndex, colIndex);
+                if (board.bitIsSet(idx, board)) {
+                    setPixel(imageData, colIndex, rowIndex, 255, 255, 255, 255)
+
+                }
+            }
+        }
+
+
+        this.canvasContext.putImageData(imageData, 0, 0);
+
+        function setPixel(imageData, x, y, r, g, b, a) {
+            var index = (x + y * imageData.width) * 4;
+            imageData.data[index + 0] = r;
+            imageData.data[index + 1] = g;
+            imageData.data[index + 2] = b;
+            imageData.data[index + 3] = a;
+        }
+    }
+}
