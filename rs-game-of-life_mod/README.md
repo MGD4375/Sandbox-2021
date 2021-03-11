58d15e4e03da5102a8580538b32d0be5afa8a295
SCMorgan <Morgan@schoolcomms.co.uk>
2021-03-11 07:53:17 +0000

    Proof that larger hashmaps will make it faster

--- rs-game-of-life_mod/README.md
+++ rs-game-of-life_mod/README.md
@@ -2,6 +2,10 @@ Documentation
 https://rustwasm.github.io/docs/wasm-bindgen/examples/without-a-bundler.html
 
 # Build
-wasm-pack build --target web
+> wasm-pack build --target web
+
+# Run Tests
+I'm not yet sure how to fix this.
+> wasm-pack test --chrome --headless
 
 That's it! This uses ES6 modules.
--- rs-game-of-life_mod/src/lib.rs
+++ rs-game-of-life_mod/src/lib.rs
@@ -1,9 +1,9 @@
-// https://codereview.stackexchange.com/questions/42718/optimize-conways-game-of-life
-
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 extern crate fixedbitset;
 use fixedbitset::FixedBitSet;
 extern crate web_sys;
+use std::collections::HashMap;
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
@@ -16,16 +16,78 @@ pub struct Universe {
     height: u32,
     cells: FixedBitSet,
     shadow_cells: FixedBitSet,
+    lookup: HashMap<FixedBitSet, bool>,
 }
 
 #[wasm_bindgen]
 impl Universe {
     pub fn new(width: u32, height: u32) -> Universe {
+        // if width % 2 != 0 || height % 2 != 0 {
+        //     panic!("Unacceptable game height or width");
+        // }
+
         let size = (width * height) as usize;
         let mut cells = FixedBitSet::with_capacity(size);
         for i in 0..size {
             cells.set(i, js_sys::Math::random() < 0.5);
         }
+
+        let mut lookup = HashMap::new();
+
+        for one in 0..=1 {
+            for two in 0..=1 {
+                for three in 0..=1 {
+                    for four in 0..=1 {
+                        for five in 0..=1 {
+                            for six in 0..=1 {
+                                for seven in 0..=1 {
+                                    for eight in 0..=1 {
+                                        for nine in 0..=1 {
+                                            let mut key = FixedBitSet::with_capacity(9);
+
+                                            key.set(0, one == 1);
+                                            key.set(1, two == 1);
+                                            key.set(2, three == 1);
+                                            key.set(3, four == 1);
+                                            key.set(4, five == 1);
+                                            key.set(5, six == 1);
+                                            key.set(6, seven == 1);
+                                            key.set(7, eight == 1);
+                                            key.set(8, nine == 1);
+
+                                            let count = one
+                                                + two
+                                                + three
+                                                + four
+                                                + six
+                                                + seven
+                                                + eight
+                                                + nine;
+
+                                            lookup.insert(
+                                                key,
+                                                match (five == 1, count) {
+                                                    (true, x) if x < 2 => false,
+                                                    (true, 2) | (true, 3) => true,
+                                                    (true, x) if x > 3 => false,
+                                                    (false, 3) => true,
+                                                    (otherwise, _) => otherwise,
+                                                },
+                                            );
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        let string = format!("{:?}", lookup);
+        let array = js_sys::Array::new();
+        array.push(&string.into());
+        web_sys::console::log(&array);
 
         let shadow_cells = cells.clone();
         Universe {
@@ -33,6 +95,7 @@ impl Universe {
             height,
             cells,
             shadow_cells,
+            lookup,
         }
     }
 
@@ -52,43 +115,44 @@ impl Universe {
         (row * self.width + column) as usize
     }
 
-    fn live_neighbor_count(&self, row: u32, col: u32) -> u8 {
-        let mut count = 0;
+    fn scan_area(&self, row: u32, col: u32) -> FixedBitSet {
+        let mut neighbours = FixedBitSet::with_capacity(9);
 
         let north = if row == 0 { self.height - 1 } else { row - 1 };
         let south = if row == self.height - 1 { 0 } else { row + 1 };
         let west = if col == 0 { self.width - 1 } else { col - 1 };
         let east = if col == self.width - 1 { 0 } else { col + 1 };
 
-        count += self.cells[self.get_index(north, west)] as u8;
-        count += self.cells[self.get_index(north, col)] as u8;
-        count += self.cells[self.get_index(north, east)] as u8;
+        //  Above
+        neighbours.set(0, self.cells[self.get_index(north, west)]);
+        neighbours.set(1, self.cells[self.get_index(north, col)]);
+        neighbours.set(2, self.cells[self.get_index(north, east)]);
 
-        count += self.cells[self.get_index(row, west)] as u8;
-        count += self.cells[self.get_index(row, east)] as u8;
-        count += self.cells[self.get_index(south, west)] as u8;
-        count += self.cells[self.get_index(south, col)] as u8;
-        count += self.cells[self.get_index(south, east)] as u8;
+        //  My row
+        neighbours.set(3, self.cells[self.get_index(row, west)]);
+        neighbours.set(4, self.cells[self.get_index(row, col)]);
+        neighbours.set(5, self.cells[self.get_index(row, east)]);
 
-        count
+        //  My col
+        neighbours.set(6, self.cells[self.get_index(south, west)]);
+        neighbours.set(7, self.cells[self.get_index(south, col)]);
+        neighbours.set(8, self.cells[self.get_index(south, east)]);
+
+        return neighbours;
     }
 
     pub fn tick(&mut self) {
-        for row in 0..self.height {
-            for col in 0..self.width {
+        for row in (0..self.height).step_by(5) {
+            for col in (0..self.width).step_by(5) {
                 let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, col);
-                self.shadow_cells.set(
-                    idx,
-                    match (cell, live_neighbors) {
-                        (true, x) if x < 2 => false,
-                        (true, 2) | (true, 3) => true,
-                        (true, x) if x > 3 => false,
-                        (false, 3) => true,
-                        (otherwise, _) => otherwise,
-                    },
-                );
+                let key = self.scan_area(row, col);
+
+                let value = self.lookup.get(&key);
+                self.shadow_cells.set(idx + 0, *value.unwrap());
+                self.shadow_cells.set(idx + 1, *value.unwrap());
+                self.shadow_cells.set(idx + 2, *value.unwrap());
+                self.shadow_cells.set(idx + 3, *value.unwrap());
+                self.shadow_cells.set(idx + 4, *value.unwrap());
             }
         }
 
